cb0d0acc8bc1d8752b61a7e795c0b66538e3f668
dannnyyyyyyyyyyyyyyyy <danymauz98@gmail.com>
2026-09-21 20:47:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ausgaben-v9';
+const CACHE = 'ausgaben-v10';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
